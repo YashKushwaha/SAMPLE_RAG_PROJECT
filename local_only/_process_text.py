fb65c664f88b1bcbc6fd0bcf35ebb0808ccf8c52
Yash Kushwaha <yash1158@gmail.com>
2025-04-18 14:50:02 +0530

    Stop tracking local_only folder

--- local_only/_process_text.py
+++
@@ -1,9 +0,0 @@
-def load_text(text_file):
-    with open(text_file, 'r') as f:
-        text = f.read()
-    return text
-
-def clean_text(text):
-    lines = [i for i in text.split('\n') if (len(i) > 20) and len(i.split() > 4)]
-    return '\n'.join(lines)
- 
